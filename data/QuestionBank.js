196d8a8c13668eba975f620679a3f8e2f0cb8770
VuTrungHieu2001 <88780876+VuTrungHieu2001@users.noreply.github.com>
2022-05-20 14:07:22 +0700

    create repo

---
+++ data/QuestionBank.js
@@ -0,0 +1,154 @@
+export const Questions =[
+    {
+        content:" Thời gian thực dân Pháp tiến hành khai thác thuộc địa lần thứ nhất ở Việt Nam khi nào ?",
+        optionA:"1858-1884",
+        optionB:"1884-1896",
+        optionC:"1896-1913",
+        optionD:"1914-1918",
+        answer : "optionC"
+    },
+    {
+        content:"Trong đợt khai thác thuộc địa lần thứ nhất của thực dân Pháp ở nước ta có giai cấp mới nào được hình thành?",
+        optionA:"Giai cấp tư sản",
+        optionB:"Giai cấp tư sản và công nhân",
+        optionC:"Giai cấp công nhân",
+        optionD:"Giai cấp tiểu tư sản",
+        answer : "optionC"
+    },
+    {
+        content:"Trước Chiến tranh thế giới thứ nhất, ở Việt Nam có những giai cấp nào",
+        optionA:"Địa chủ phong kiến và nông dân",
+        optionB:"Địa chủ phong kiến, nông dân, tư sản, tiểu tư sản và công nhân",
+        optionC:" Địa chủ phong kiến, nông dân và công nhân ",
+        optionD:"Địa chủ phong kiến, nông dân và tiểu tư sản",
+        answer : "optionC"
+    },
+    {
+        content:"Dưới chế độ thực dân phong kiến, giai cấp nông dân Việt Nam có yêu cầu bức thiết nhất là gì?",
+        optionA:"Độc lập dân tộc",
+        optionB:"Ruộng đất",
+        optionC:"Quyền bình đẳng nam, nữ",
+        optionD:"Được giảm tô, giảm tức",
+        answer : "optionA"
+    },
+    {
+        content:"Mâu thuẫn cơ bản và chủ yếu ở Việt Nam đầu thế kỷ XX là mâu thuẫn nào?",
+        optionA:"Mâu thuẫn giữa giai cấp nông dân với giai cấp địa chủ phong kiếnc",
+        optionB:"Mâu thuẫn giữa giai cấp công nhân với giai cấp tư sản",
+        optionC:"Mâu thuẫn giữa công nhân và nông dân với đế quốc và phong kiến",
+        optionD:"Mâu thuẫn giữa dân tộc Việt Nam với đế quốc xâm lược và tay sai của chúng",
+        answer : "optionD"
+    },
+    {
+        content:"Đặc điểm ra đời của giai cấp công nhân Việt Nam như thế nào",
+        optionA:"Ra đời trước giai cấp tư sản, trong cuộc khai thác thuộc địa lần thứ nhất của thực dân Pháp",
+        optionB:"Phần lớn xuất thân từ nông dân.",
+        optionC:"Chịu sự áp bức và bóc lột của đế quốc, phong kiến và tư sản",
+        optionD:"Cả a, b và c",
+        answer : "optionA"
+    },
+    {
+        content:"Những giai cấp bị trị ở Việt Nam dưới chế độ thuộc địa của đế quốc Pháp là:",
+        optionA:"Công nhân và nông dân",
+        optionB:"Công nhân, nông dân, tiểu tư sản",
+        optionC:"Công nhân, nông dân, tiểu tư sản, tư sản dân tộc",
+        optionD:"Công nhân, nông dân, tiểu tư sản, tư sản dân tộc, địa chủ vừa và nhỏ ",
+        answer : "optionD"
+    },
+    {
+        content:"Khi nào phong trào công nhân Việt Nam hoàn toàn trở thành một phong trào tự giác?",
+        optionA:"Năm 1920 (tổ chức công hội ở Sài Gòn được thành lập)",
+        optionB:"Năm 1925 (cuộc bãi công Ba Son)",
+        optionC:"Năm 1929 (sự ra đời ba tổ chức cộng sản)",
+        optionD:"Năm 1930 (Đảng Cộng sản Việt Nam ra đời) ",
+        answer : "optionD"
+    },
+    {
+        content:"Nguyễn ái Quốc lựa chọn con đường giải phóng dân tộc theo khuynh hướng chính trị vô sản vào thời gian nào?",
+        optionA:"1917",
+        optionB:"1918",
+        optionC:"1919",
+        optionD:"1920 ",
+        answer : "optionD"
+    },
+    {
+        content:"Báo Đời sống công nhân là của tổ chức nào?",
+        optionA:"Đảng Xã hội Pháp",
+        optionB:"Đảng Cộng sản Pháp",
+        optionC:"Tổng Liên đoàn Lao động Pháp",
+        optionD:"Hội Liên hiệp thuộc địa",
+        answer : "optionC"
+    },
+    {
+        content:"Hội Liên hiệp thuộc địa được thành lập vào năm nào?",
+        optionA:"1920",
+        optionB:"1921",
+        optionC:"1923",
+        optionD:"1924",
+        answer : "optionB"
+    },
+    {
+        content:"Nguyễn ái Quốc đã đọc Sơ thảo lần thứ nhất Luận cương về vấn đề dân tộc và vấn đề thuộc địa khi nào? ở đâu??",
+        optionA:"7/ 1920 - Liên Xô",
+        optionB:"7/ 1920 - Pháp",
+        optionC:"7/1920 - Quảng Châu (Trung Quốc)",
+        optionD:"8/1920 - Trung Quốc",
+        answer : "optionB"
+    },
+    {
+        content:"Sự kiện nào được Nguyễn ái Quốc đánh giá 'như chim én nhỏ báo hiệu mùa Xuân'?",
+        optionA:"Cách mạng tháng Mười Nga bùng nổ và thắng lợi",
+        optionB:"Sự thành lập Đảng Cộng sản Pháp",
+        optionC:" Vụ mưu sát tên toàn quyền Méclanh của Phạm Hồng Thái",
+        optionD:"Sự thành lập Hội Việt Nam cách mạng thanh niên",
+        answer : "optionC"
+    },
+    {
+        content:"Phong trào đòi trả tự do cho cụ Phan Bội Châu diễn ra sôi nổi năm nào?",
+        optionA:"1924",
+        optionB:"1925 ",
+        optionC:"1926",
+        optionD:"1927",
+        answer : "optionB"
+    },
+    {
+        content:" Nguyễn ái Quốc từ Liên Xô về Quảng Châu (Trung Quốc) vào thời gian nào?",
+        optionA:"12/1924",
+        optionB:"12/1925",
+        optionC:"11/1924",
+        optionD:"10/1924",
+        answer : "optionA"
+    },
+    {
+        content:"Hội Việt Nam Cách mạng Thanh niên thực hiện chủ trương 'vô sản hoá' khi nào?",
+        optionA:"Cuối năm 1926 đầu năm 1927",
+        optionB:"Cuối năm 1927 đầu năm 1928",
+        optionC:"Cuối năm 1928 đầu năm 1929 ",
+        optionD:"Cuối năm 1929 đầu năm 1930",
+        answer : "optionC"
+    },
+    {
+        content:"Tên chính thức của tổ chức này được đặt tại Đại hội lần thứ nhất ở Quảng Châu (tháng 5-1929) là gì?",
+        optionA:" Việt Nam Thanh niên cách mạng đồng chí Hội",
+        optionB:"Hội Việt Nam cách mạng đồng minh",
+        optionC:"Hội Việt Nam độc lập đồng minh",
+        optionD:"Hội Việt Nam Cách mạng Thanh niên",
+        answer : "optionD"
+    },
+    {
+        content:"Việt Nam Quốc dân Đảng được thành lập vào thời gian nào?",
+        optionA:"12/1927 ",
+        optionB:"11/1926",
+        optionC:"8/1925",
+        optionD:"7/1925",
+        answer : "optionA"
+    },
+    {
+        content:"Khởi nghĩa Yên Bái nổ ra vào thời gian nào?",
+        optionA:"9-2-1930 ",
+        optionB:"9-3-1930",
+        optionC:"3-2-1930",
+        optionD:"9-3-1931",
+        answer : "optionA"
+    },
+]
